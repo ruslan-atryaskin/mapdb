b515b07785aa9402c261611bba4da375a0d263b0
nbauma109 <nbauma109@github>
2017-09-04 06:46:48 +0200

    new serializer, new tests

--- src/test/java/org/mapdb/SerializerFSTLZ4Test.java
+++ src/test/java/org/mapdb/SerializerFSTLZ4Test.java
@@ -6,7 +6,7 @@ import org.mapdb.serializer.SerializerFSTLZ4;
 public class SerializerFSTLZ4Test extends TestCase {
 
     public void test() {
-        SerializerFSTLZ4<SerializableTestObject> serializerFSTLZ4 = new SerializerFSTLZ4<>(100);
+        SerializerFSTLZ4<SerializableTestObject> serializerFSTLZ4 = new SerializerFSTLZ4<>(165);
         DBConcurrentMap<String, SerializableTestObject> map = DBMaker.memoryDB().make().hashMap("map", Serializer.STRING, serializerFSTLZ4).createOrOpen();
         for (int i = 0; i < 1000000; i++) {
             SerializableTestObject obj = new SerializableTestObject();
